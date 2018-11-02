36877a987095f11558d06797d585f3ffd8be998c
Donovan <sales@novo.io>
2018-11-02 11:13:22 +0800

    Updates

---
+++ precache-manifest.205e186fffd8b0be07726be3519df968.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6d0aa42199878ab9859",
+    "url": "/react-tutorial/static/js/runtime~main.d6d0aa42.js"
+  },
+  {
+    "revision": "14e0a7235e934ab32b4a",
+    "url": "/react-tutorial/static/js/main.14e0a723.chunk.js"
+  },
+  {
+    "revision": "5b966dc2e69f15274453",
+    "url": "/react-tutorial/static/js/1.5b966dc2.chunk.js"
+  },
+  {
+    "revision": "14e0a7235e934ab32b4a",
+    "url": "/react-tutorial/static/css/main.05818a20.chunk.css"
+  },
+  {
+    "revision": "9987438834e837dcd3ca8f9db1d17c9d",
+    "url": "/react-tutorial/index.html"
+  }
+];
